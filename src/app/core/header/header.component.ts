4ef427345fee4e434ccbcf5aaa9065531b2b3aef
Seva <zimovetsva@gmail.com>
2025-05-23 19:14:42 +0400

    first commit + header

---
+++ src/app/core/header/header.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  isFavorite = false;
+  cartItemsCount = 0;
+  logoContent = `<svg width="227" height="226" viewBox="0 0 227 226" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0.817383 122.437L113.676 150.543L226.535 122.437V194.112L113.676 225.717L0.817383 194.112V122.437Z" fill="#D1DEED"/>
+<path d="M226.535 103.28L113.676 75.1744L0.817429 103.28V31.6049L113.676 7.62939e-06L226.535 31.6049V103.28Z" fill="#D1DEED"/>
+<path d="M226.535 122.624L103.915 77.4601L0.817383 103.094L123.437 148.257L226.535 122.624Z" fill="#91A8D3"/>
+</svg>`
+  // Поиск товаров
+  onSearch(query: string) {
+    console.log('Поиск:', query);
+    // Здесь можно добавить логику поиска (например, через сервис)
+  }
+
+  // Избранное
+  toggleFavorites() {
+    this.isFavorite = !this.isFavorite;
+  }
+
+  // Корзина
+  openCart() {
+    console.log('Открыть корзину');
+    // Переход на страницу корзины или открытие модального окна
+  }
+}
